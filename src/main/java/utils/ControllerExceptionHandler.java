1109938370bb2de8f7c1c4347d400ff774263101
Kevin Khanda <kev100500@gmail.com>
2016-06-19 16:15:36 +0300

    Class packaged local

--- src/main/java/utils/ControllerExceptionHandler.java
+++ src/main/java/utils/ControllerExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 
 @Controller
-public class ControllerExceptionHandler {
+class ControllerExceptionHandler {
 
     private static final Logger log = Logger.getLogger(
             ControllerExceptionHandler.class);
