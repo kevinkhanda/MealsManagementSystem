51dce15f234cb057e57ef9f4ac9ba85daa0bea6d
Kevin Khanda <kev100500@gmail.com>
2016-06-19 18:16:28 +0300

    Class for determining data structure.

---
+++ src/main/java/utils/Ajax.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Kevin Khanda on 6/19/2016.
+ * This class will determine structure of data to send.
+ */
+public class Ajax {
+
+    public static Map<String, Object> successResponce(Object object) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("result", "success");
+        response.put("data", object);
+        return response;
+    }
+
+    public static Map<String, Object> emptyResponse() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("result", "success");
+        return response;
+    }
+
+    public static Map<String, Object> errorResponse(String errorMessage){
+        Map<String, Object> response = new HashMap<>();
+        response.put("result", "error");
+        response.put("message", errorMessage);
+        return response;
+    }
+}
