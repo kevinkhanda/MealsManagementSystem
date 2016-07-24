e9575f281ab8f8f2855567b52a5111bd05619bbc
Kevin Khanda <kev100500@gmail.com>
2016-07-24 16:27:37 +0300

    Finished with all repositories.

--- src/main/java/repository/menu/MenuRepositoryImpl.java
+++ src/main/java/repository/menu/MenuRepositoryImpl.java
@@ -3,9 +3,11 @@ package repository.menu;
 import entity.Menu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import java.io.File;
 import java.sql.Date;
+import java.sql.Types;
 
 /**
  * Created by Kevin Khanda on 7/24/2016.
@@ -18,21 +20,37 @@ public class MenuRepositoryImpl implements MenuRepository<Menu>{
 
     @Override
     public String getOrgName(Menu object) {
-        return null;
+        String result;
+        SqlRowSet rowSet = jdbcOperations.queryForRowSet("SELECT organisation FROM menu" +
+                "WHERE id = ?;", object.getId());
+        result = rowSet.getString("organisation");
+        return result;
     }
 
     @Override
     public Date getMenuDate(Menu object) {
-        return null;
+        Date result;
+        SqlRowSet rowSet = jdbcOperations.queryForRowSet("SELECT date FROM menu" +
+                "WHERE id = ?;", object.getId());
+        result = rowSet.getDate("date");
+        return result;
     }
 
     @Override
     public File getMenu(Menu object) {
-        return null;
+        File result;
+        SqlRowSet rowSet = jdbcOperations.queryForRowSet("SELECT menu FROM menu" +
+                "WHERE id = ?;", object.getId());
+        result = (File) rowSet.getObject("menu");
+        return result;
     }
 
     @Override
     public void addMenu(Menu object) {
-
+        Object[] params = new Object[] {object.getId(), object.getOrganisation(),
+            object.getDate(), object.getMenu()};
+        int[] types = new int[] {Types.INTEGER, Types.VARCHAR, Types.DATE, Types.BLOB};
+        jdbcOperations.update("INSERT INTO menu (id, organisation, date, menu)" +
+                "VALUES (?, ?, ?, ?);", params, types);
     }
 }
