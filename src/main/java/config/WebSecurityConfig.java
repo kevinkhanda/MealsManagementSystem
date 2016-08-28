4dedb84c92eaca901ca9dbcf3d4cb59962640238
Альберт <97.10.21@mail.ru>
2016-08-28 22:54:55 +0300

    Static resources are working now

--- src/main/java/config/WebSecurityConfig.java
+++ src/main/java/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -30,4 +31,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/*");
+        web.ignoring().antMatchers("/js/*");
+        web.ignoring().antMatchers("/img/*");
+        web.ignoring().antMatchers("/fonts/*");
+    }
+
 }
